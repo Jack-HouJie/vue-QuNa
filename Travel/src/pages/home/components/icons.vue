8ce20c88c792a8d244bcd9c6b75d68f440809c55
HouJie <houjie_jack@outlook.com>
2020-06-13 02:08:44 +0800

    完成图标区域布局

---
+++ Travel/src/pages/home/components/icons.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content"
+             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
+      </div>
+      <p class="icon-desc">民宿客栈</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.icons {
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  float: left;
+  overflow: hidden;
+  height: 0;
+  width: 25%;
+  padding-bottom: 25%;
+}
+
+.icon-img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0.44rem;
+  box-sizing: border-box;
+  padding: 0.1rem;
+}
+
+.icon-img-content {
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+}
+
+.icon-desc {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 0.44rem;
+  line-height: 0.44rem;
+  color: $darkTextColor;
+  text-align: center;
+}
+</style>
--- Travel/src/pages/home/home.vue
+++ Travel/src/pages/home/home.vue
@@ -5,6 +5,7 @@
       <!-- 使用子组件 -->
       <home-header></home-header>
       <HmoeSwiper></HmoeSwiper>
+      <HmoeIcons></HmoeIcons>
     </div>
   </div>
 </template>
@@ -13,12 +14,14 @@
 // 引入子组件
 import HomeHeader from './components/header'
 import HmoeSwiper from './components/swiper'
+import HmoeIcons from './components/icons'
 export default {
   name: 'home',
   // 声明局部组件
   components: {
     HomeHeader,
-    HmoeSwiper
+    HmoeSwiper,
+    HmoeIcons
   }
 }
 </script>
