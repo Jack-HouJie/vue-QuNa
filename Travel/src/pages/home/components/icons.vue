69462a79866e00e984c1b76baab64e36d66ab662
HouJie <houjie_jack@outlook.com>
2020-06-13 15:13:20 +0800

    添加图标区域部分逻辑

--- Travel/src/pages/home/components/icons.vue
+++ Travel/src/pages/home/components/icons.vue
@@ -1,73 +1,98 @@
 <template>
   <div class="icons">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content"
-             src="https://s.qunarzz.com/homenode/images/touchheader/hotel.png">
-      </div>
-      <p class="icon-desc">民宿客栈</p>
-    </div>
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages"
+                    :key="index">
+        <div class="icon"
+             v-for="item of page"
+             :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content"
+                 :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店酒店酒店酒店酒店酒店'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0005',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0006',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0007',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0008',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '0009',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店9'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.icons {
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>> .swiper-container {
   overflow: hidden;
   height: 0;
   padding-bottom: 50%;
@@ -107,5 +132,6 @@ export default {
   line-height: 0.44rem;
   color: $darkTextColor;
   text-align: center;
+  ellipsis();
 }
 </style>
