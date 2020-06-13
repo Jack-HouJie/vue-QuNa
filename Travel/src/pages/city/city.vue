97a357627f4177ade9038e580e07a75431675150
HouJie <houjie_jack@outlook.com>
2020-06-14 01:32:12 +0800

    完成城市也列表部分基本样式

--- Travel/src/pages/city/city.vue
+++ Travel/src/pages/city/city.vue
@@ -2,17 +2,20 @@
   <div>
   <CityHeader></CityHeader>
   <CitySearch></CitySearch>
+  <CityList></CityList>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/header'
 import CitySearch from './components/search'
+import CityList from './components/list'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList
   }
 }
 </script>
---
+++ Travel/src/pages/city/components/list.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="city-item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">B</div>
+      <div class="city-item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">C</div>
+      <div class="city-item-list">
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+        <div class="item border-bottom">阿拉尔</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color #ccc
+  &:after
+    border-color #ccc
+.border-bottom
+  &:before
+    border-color #ccc
+.list
+  overflow hidden
+  position absolute
+  top 1.58rem
+  right 0
+  bottom 0
+  left 0
+  .title
+    line-height 0.44rem
+    font-size 0.26rem
+    background #eee
+    padding-left 0.2rem
+    color #666
+  .button-list
+    overflow hidden // 触发BFC
+    padding 0.1rem 0.6rem 0.1rem 0.1rem
+    .button-wrapper
+      float left
+      width 33.33%
+      .button
+        text-align center
+        margin 0.1rem
+        padding 0.1rem
+        border 0.02rem solid #ccc
+        border-radius 0.06rem
+  .city-item-list
+    .item
+      line-height 0.76rem
+      padding-left 0.2rem
+</style>
