82be39819c95bd806d4b61b5e1c07d171054e7af
HouJie <houjie_jack@outlook.com>
2020-06-16 19:46:33 +0800

    修复headerDetail优先级BUG，完成list递归组件

---
+++ Travel/src/pages/detail/components/list.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="item"
+         v-for="(item,index) of list"
+         :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div class="item-children"
+           v-if="item.children">
+        <DetailList :list="item.children"></DetailList>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.item-title-icon
+  position relative
+  left 0.06rem
+  top 0.06rem
+  display inline-block
+  width 0.36rem
+  height 0.36rem
+  background $bgColor
+  margin-right 0.1rem
+  background-size 0.4rem 3rem
+.item-title
+  line-height 0.8rem
+  font-size 0.32rem
+  padding 0 0.2rem
+.item-children
+  padding 0 0.2rem
+</style>
--- Travel/src/pages/detail/detail.vue
+++ Travel/src/pages/detail/detail.vue
@@ -2,6 +2,7 @@
   <div>
     <DetailBanner></DetailBanner>
     <DetailHeader></DetailHeader>
+    <DetailList :list="list"></DetailList>
     <div class="content"></div>
   </div>
 </template>
@@ -9,11 +10,33 @@
 <script>
 import DetailBanner from './components/banner'
 import DetailHeader from './components/header'
+import DetailList from './components/list'
+
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '学生票',
+        children: [{
+          title: '学生三馆联票',
+          children: [{
+            title: '学生三馆联票套餐一'
+          }, {
+            title: '学生三馆联票套餐二'
+          }]
+        }, {
+          title: '学生五馆联票'
+        }]
+      }, {
+        title: '成人票'
+      }]
+    }
   }
 }
 </script>
