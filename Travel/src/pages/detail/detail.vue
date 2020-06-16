2f1326767275c17e5a8b0480737fcacf34c22e0a
HouJie <houjie_jack@outlook.com>
2020-06-16 17:16:18 +0800

    完成详情页Header组件

--- Travel/src/pages/detail/detail.vue
+++ Travel/src/pages/detail/detail.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/banner'
+import DetailHeader from './components/header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
-
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.content
+  height 50rem
+</style>
